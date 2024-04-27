3c871e5b9c2be06241c62cc8c8edfd5bfbc63eb2
Utsav1o1 <utsavkarki244@gmail.com>
2024-04-27 12:19:03 +0545

    refactor the code and added css

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+
+@section('title', 'The list of tasks')
+@section('content')
+
+<nav class="mb-4">
+
+    <a class="link" href="{{ route('tasks.create') }}">Add Task!</a>
+</nav>
+    
+
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}" @class(['line-through' => $task->completed])>{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>There is no task</div>
+    @endforelse
+
+    @if ($tasks->count())
+        <nav class="mt-4"> 
+
+            {{ $tasks->links() }}
+        </nav>
+    @endif
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style type="text/tailwindcss">
+        .btn {
+            @apply rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50
+        }
+
+        .link {
+            @apply font-medium text-gray-700 underline decoration-yellow-500
+        }
+
+        label {
+            @apply block uppercase text-slate-700 mb-2
+        }
+
+        input,
+        textarea {
+            @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none
+        }
+
+        .error {
+            @apply text-red-500 text-sm
+        }
+    </style>
+    <title>Document</title>
+    @yield('styles')
+</head>
+
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
+    <h1 class="mb-4 text-2xl">@yield('title')</h1>
+    <div>
+
+        @if (session()->has('success'))
+            <div>{{ session('success') }}</div>
+        @endif
+
+        @yield('content')
+    </div>
+</body>
+
+</html>
